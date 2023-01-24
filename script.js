94f8a7925c24c3393c098d6cf98232bbb1f67017
Milena <mitessari@yahoo.com.br>
2023-01-24 22:49:28 +0000

    added a function printTodoData to print in the web page the content, added a function handleProjectFormSubmit to set a value to the element from the list, added a event listener todolist.on to submit the content from the input area

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-var todoList = $("#todoList");
+ var todoList = $("#todoList");
 var button = $("#submitBtn");
 var todoListForm = $("#todoList-form");
+
+
+function printTodoData(name) {
+  var projectRowEl = $('<tr>');
+
+  var projectNameTdEl = $('<td>').addClass('p-2').text(name); 
+
+  projectRowEl.append(
+    projectNameTdEl );
+
+
+  }
+
+  function handleProjectFormSubmit(event) {
+    event.preventDefault();
+  var projectName = projectNameInputEl.val().trim();
+  printTodoData(projectName); }
+
+
+  todoListForm.on('submit', handleProjectFormSubmit);
+
+
 
 setInterval(
     function () {
@@ -8,15 +30,30 @@ setInterval(
 
     }, 1000)
 
-    todoListForm.on("submit", function(event){
-        event.preventDefault();
-        $newLi = $("<li>")
-        let todoItemText = $("input").val()
-        $newLi.text(todoItemText)
-        todoListForm.append($newLi)
-      })
+    // todoListForm.on("submit", function(event){
+    //     event.preventDefault();
+       
+    //   })
+
+    // todoListForm.on("submit", function(event){
+    //     event.preventDefault();
+    //     todoList.append(
+    //        todoListForm.value )
+        
+      
+    // })
+
+//     $("#submitBtn").on("click", function (event) {
+//         todoListForm.innerHTML = "";
+//         event.preventDefault();
+//         todoListForm.append(
+
+//               todoListForm.val()
 
 
+// )});
+
+//                   var storedTodo = localStorage.getItem("todoList-form");
 
 
 
